419bc030e688529d2af1b6d1809c4f5c5ae2e4f3
Withanage <dulip.withanage@tib.eu>
2022-03-25 16:06:24 +0100

    bigfix

--- RosettaExportDeployment.inc.php
+++ RosettaExportDeployment.inc.php
@@ -165,7 +165,7 @@ class RosettaExportDeployment
 		$response = curl_exec($ch);
 		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		$sipIdNode = $this->getSipIdNode($response);
-
+		echo($response);
 		if ($response_code == 200 && !is_null($sipIdNode)) {
 			$submissionDao = DAORegistry::getDAO('SubmissionDAO');
 			$submission->setData('dateUpdated', Core::getCurrentDate());
