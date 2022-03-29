020f26cc705c460ae5595636ce90aa93853fe80e
Withanage <dulip.withanage@tib.eu>
2022-03-29 09:51:31 +0200

    Unit Tests

--- tests/functional/FunctionalRosettaExportTestCase.php
+++ tests/functional/FunctionalRosettaExportTestCase.php
@@ -1,46 +1,261 @@
 <?php
 
 
+require_mock_env('env2');
+
+
 import('plugins.importexport.rosetta.RosettaExportPlugin');
 import('lib.pkp.tests.plugins.PluginTestCase');
+
+require_mock_env('env2');
+
+import('lib.pkp.tests.PKPTestCase');
+
+import('lib.pkp.classes.oai.OAIStruct');
+import('lib.pkp.classes.oai.OAIUtils');
+import('plugins.oaiMetadataFormats.dc.OAIMetadataFormat_DC');
+import('plugins.oaiMetadataFormats.dc.OAIMetadataFormatPlugin_DC');
+
+import('lib.pkp.classes.services.PKPSchemaService'); // Constants
+
 
 
 class FunctionalRosettaExportTest extends PluginTestCase {
 
-	protected function setUp() : void {
-		$this->pluginId = 'rosetta';
 
 
-		$this->defaultPluginSettings = array(
-			'username' => '',
-			'password' => '',
-			'registrantName' => 'Registrant',
-			'fromCompany' => 'From Company',
-			'fromName' => 'From Person',
-			'fromEmail' => 'from@email.com',
-			'publicationCountry' => 'US',
-			'exportIssuesAs' => O4DOI_ISSUE_AS_WORK
-		);
 
-		parent::setUp('1749');
-	}
-	public function testExportRosettaExport() {
-		$exportPages = array('issues', 'articles', 'galleys', 'all');
-
-		$pluginSettingsDao = DAORegistry::getDAO('PluginSettingsDAO'); /* @var $pluginSettingsDao PluginSettingsDAO */
-		$pluginSettingsDao->updateSetting(1, $this->pluginId . 'exportplugin', 'registrantName', '');
-		$stack = [];
-		$this->assertSame(0, count($stack));
-
-		array_push($stack, 'foo');
-		$this->assertSame('foo', $stack[count($stack)-1]);
-		$this->assertSame(1, count($stack));
-
-		$this->assertSame('foo', array_pop($stack));
-		$this->assertSame(0, count($stack));
-		$this->assertConfigurationError($exportPages, 'The plug-in is not fully set up');
+	/**
+	 * @see PKPTestCase::getMockedDAOs()
+	 */
+	protected function getMockedDAOs()
+	{
+		return array('AuthorDAO', 'OAIDAO', 'ArticleGalleyDAO');
 	}
 
+	/**
+	 * @see PKPTestCase::getMockedRegistryKeys()
+	 */
+	protected function getMockedRegistryKeys()
+	{
+		return array('request');
+	}
 
+	/**
+	 * @covers OAIMetadataFormat_DC
+	 * @covers Dc11SchemaArticleAdapter
+	 */
+	public function testToXml()
+	{
+		#$this->markTestSkipped('Skipped because of weird class interaction with ControlledVocabDAO.');
+
+		//
+		// Create test data.
+		//
+		$journalId = 1;
+
+		// Enable the DOI plugin.
+		$pluginSettingsDao = DAORegistry::getDAO('PluginSettingsDAO');
+		/* @var $pluginSettingsDao PluginSettingsDAO */
+		$pluginSettingsDao->updateSetting($journalId, 'doipubidplugin', 'enabled', 1);
+		$pluginSettingsDao->updateSetting($journalId, 'doipubidplugin', 'enableIssueDoi', 1);
+		$pluginSettingsDao->updateSetting($journalId, 'doipubidplugin', 'enablePublicationDoi', 1);
+		$pluginSettingsDao->updateSetting($journalId, 'doipubidplugin', 'enableRepresentationyDoi', 1);
+
+		// Author
+		import('classes.article.Author');
+		$author = new Author();
+		$author->setGivenName('author-firstname', 'en_US');
+		$author->setFamilyName('author-lastname', 'en_US');
+		$author->setAffiliation('author-affiliation', 'en_US');
+		$author->setEmail('someone@example.com');
+
+		// Article
+		import('classes.submission.Submission');
+		$article = $this->getMockBuilder(Submission::class)
+			->setMethods(array('getBestId'))
+			->getMock();
+		$article->expects($this->any())
+			->method('getBestId')
+			->will($this->returnValue(9));
+		$article->setId(9);
+		$article->setJournalId($journalId);
+		$author->setSubmissionId($article->getId());
+		$article->setPages(15);
+		$article->setType('art-type', 'en_US');
+		$article->setTitle('article-title-en', 'en_US');
+		$article->setTitle('article-title-de', 'de_DE');
+		$article->setDiscipline('article-discipline', 'en_US');
+		$article->setSubject('article-subject', 'en_US');
+		$article->setAbstract('article-abstract', 'en_US');
+		$article->setSponsor('article-sponsor', 'en_US');
+		$article->setStoredPubId('doi', 'article-doi');
+		$article->setLanguage('en_US');
+
+		// Galleys
+		import('classes.article.ArticleGalley');
+		$galley = new ArticleGalley();
+		$galley->setId(98);
+		$galley->setStoredPubId('doi', 'galley-doi');
+		$galleys = array($galley);
+
+		// Journal
+		import('classes.journal.Journal');
+		$journal = $this->getMockBuilder(Journal::class)
+			->setMethods(array('getSetting'))
+			->getMock();
+		$journal->expects($this->any())
+			->method('getSetting') // includes getTitle()
+			->will($this->returnCallback(array($this, 'getJournalSetting')));
+		$journal->setPrimaryLocale('en_US');
+		$journal->setPath('journal-path');
+		$journal->setId($journalId);
+
+		// Section
+		import('classes.journal.Section');
+		$section = new Section();
+		$section->setIdentifyType('section-identify-type', 'en_US');
+
+		// Issue
+		import('classes.issue.Issue');
+		$issue = $this->getMockBuilder(Issue::class)
+			->setMethods(array('getIssueIdentification'))
+			->getMock();
+		$issue->expects($this->any())
+			->method('getIssueIdentification')
+			->will($this->returnValue('issue-identification'));
+		$issue->setId(96);
+		$issue->setDatePublished('2010-11-05');
+		$issue->setStoredPubId('doi', 'issue-doi');
+		$issue->setJournalId($journalId);
+
+
+		//
+		// Create infrastructural support objects
+		//
+
+		// Router
+		import('lib.pkp.classes.core.PKPRouter');
+		$router = $this->getMockBuilder(PKPRouter::class)
+			->setMethods(array('url'))
+			->getMock();
+		$application = Application::get();
+		$router->setApplication($application);
+		$router->expects($this->any())
+			->method('url')
+			->will($this->returnCallback(array($this, 'routerUrl')));
+
+		// Request
+		import('classes.core.Request');
+		$request = $this->getMockBuilder(Request::class)
+			->setMethods(array('getRouter'))
+			->getMock();
+		$request->expects($this->any())
+			->method('getRouter')
+			->will($this->returnValue($router));
+		Registry::set('request', $request);
+
+
+		//
+		// Create mock DAOs
+		//
+
+		// Create a mocked AuthorDAO that returns our test author.
+		import('classes.article.AuthorDAO');
+		$authorDao = $this->getMockBuilder(AuthorDAO::class)
+			->setMethods(array('getBySubmissionId'))
+			->getMock();
+		$authorDao->expects($this->any())
+			->method('getBySubmissionId')
+			->will($this->returnValue(array($author)));
+		DAORegistry::registerDAO('AuthorDAO', $authorDao);
+
+		// Create a mocked OAIDAO that returns our test data.
+		import('classes.oai.ojs.OAIDAO');
+		$oaiDao = $this->getMockBuilder(OAIDAO::class)
+			->setMethods(array('getJournal', 'getSection', 'getIssue'))
+			->getMock();
+		$oaiDao->expects($this->any())
+			->method('getJournal')
+			->will($this->returnValue($journal));
+		$oaiDao->expects($this->any())
+			->method('getSection')
+			->will($this->returnValue($section));
+		$oaiDao->expects($this->any())
+			->method('getIssue')
+			->will($this->returnValue($issue));
+		DAORegistry::registerDAO('OAIDAO', $oaiDao);
+
+		// Create a mocked ArticleGalleyDAO that returns our test data.
+		import('classes.article.ArticleGalleyDAO');
+		$articleGalleyDao = $this->getMockBuilder(ArticleGalleyDAO::class)
+			->setMethods(array('getBySubmissionId'))
+			->getMock();
+		$articleGalleyDao->expects($this->any())
+			->method('getBySubmissionId')
+			->will($this->returnValue($galleys));
+		DAORegistry::registerDAO('ArticleGalleyDAO', $articleGalleyDao);
+		// FIXME: ArticleGalleyDAO::getBySubmissionId returns iterator; array expected here. Fix expectations.
+
+		//
+		// Test
+		//
+
+		// OAI record
+		$record = new OAIRecord();
+		$record->setData('article', $article);
+		$record->setData('galleys', $galleys);
+		$record->setData('journal', $journal);
+		$record->setData('section', $section);
+		$record->setData('issue', $issue);
+
+		// Instantiate the OAI meta-data format.
+
+
+		#$xml = $mdFormat->toXml($record);
+		self::assertXmlStringEqualsXmlFile('tests/plugins/oaiMetadataFormats/dc/expectedResult.xml', $xml);
+	}
+
+
+	//
+	// Public helper methods
+	//
+	/**
+	 * Callback for journal settings.
+	 * @param $settingName string
+	 */
+	function getJournalSetting($settingName)
+	{
+		switch ($settingName) {
+			case 'name':
+				return array('en_US' => 'journal-title');
+
+			case 'licenseTerms':
+				return array('en_US' => 'journal-copyright');
+
+			case 'publisherInstitution':
+				return array('journal-publisher');
+
+			case 'onlineIssn':
+				return 'onlineIssn';
+
+			case 'printIssn':
+				return null;
+
+			default:
+				self::fail('Required journal setting is not necessary for the purpose of this test.');
+		}
+	}
+
+
+	//
+	// Private helper methods
+	//
+	/**
+	 * Callback for router url construction simulation.
+	 */
+	function routerUrl($request, $newContext = null, $handler = null, $op = null, $path = null)
+	{
+		return $handler . '-' . $op . '-' . implode('-', $path);
+	}
 }
-
