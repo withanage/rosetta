194cf881b58b83136c9845a37c56b886d3fca926
Withanage <dulip.withanage@tib.eu>
2022-04-05 16:15:26 +0200

    Tests Functional

--- tests/data/JournalTest.inc.php
+++ tests/data/JournalTest.inc.php
@@ -9,23 +9,44 @@ class JournalTest
         $this->functionalRosettaExportTest = $functionalRosettaExportTest;
     }
 
+
+	public function  createPublication(Journal $context, Submission $submission) {
+		$publicationDao = DAORegistry::getDAO('PublicationDAO');
+		$publication = $publicationDao->newDataObject(); /** @var $publication PKPPublication */
+
+		$publication->setData('submissionId', $submission->getId());
+
+		$publication->stampModified();
+
+		if (empty($publicationLocale))
+			$publicationLocale = $context->getPrimaryLocale();
+
+		$publication->setData('id', 1);
+		$publication->setData('locale', $publicationLocale);
+		$publication->setData('version', 'version');
+		$publication->setData('seq', 'seq');
+		$publication->setData('accessStatus', 'access_status');
+		$publication->setData('status', 'status');
+		$publication->setData('primaryContactId', 1);
+		$publication->setData('urlPath', 'url_path');
+		return $publication;
+
+	}
+
     /**
      * @param string $primaryLocale
      * @return Section
      */
-    public function createSections(string $primaryLocale): Section
+    public function createSection(Journal $context): Section
     {
         import('classes.journal.Section');
         $section = new Section();
-        $section->setIdentifyType('section-identify-type', $primaryLocale);
-        return $section;
+        $section->setIdentifyType('section-identify-type', $context->getPrimaryLocale());
+		return $section;
     }
 
-    /**
-     * @param int $journalId
-     * @return Issue|mixed|\PHPUnit\Framework\MockObject\MockObject
-     */
-    public function createIssues(int $journalId)
+
+    public function createIssue(Journal $context) :Issue
     {
         import('classes.issue.Issue');
         $issue = $this->functionalRosettaExportTest->getMockBuilder(Issue::class)
@@ -37,20 +58,27 @@ class JournalTest
         $issue->setId(96);
         $issue->setDatePublished('2010-11-05');
         $issue->setStoredPubId('doi', 'issue-doi');
-        $issue->setJournalId($journalId);
+        $issue->setJournalId($context->getId());
         return $issue;
     }
 
     /**
      * @return ArticleGalley[]
      */
-    public function setGalleys(): array
+    public function createGalleys(Submission  $submission): array
     {
-        import('classes.article.ArticleGalley');
-        $galley = new ArticleGalley();
-        $galley->setId(98);
-        $galley->setStoredPubId('doi', 'galley-doi');
-        $galleys = array($galley);
+		$galleys = [] ;
+		import('classes.article.ArticleGalleyDAO');
+		$articleGalleyDao = $this->functionalRosettaExportTest->getMockBuilder(ArticleGalleyDAO::class)
+			->setMethods(array('getBySubmissionId'))
+			->getMock();
+		DAORegistry::registerDAO('ArticleGalleyDAO', $articleGalleyDao);
+		import('classes.article.ArticleGalley');
+		$galley = new ArticleGalley();
+		$galley->setId(98);
+		$galley->setData('publicationId',$submission->getLatestPublication()->getId());
+		$galley->setStoredPubId('doi', 'galley-doi');
+		$galleys[] = $galley;
         return $galleys;
     }
 
@@ -83,36 +111,29 @@ class JournalTest
         return $context;
     }
 
-    /**
-     * @param array $galleys
-     */
-    public function createGalleys(array $galleys): void
-    {
-// Create a mocked ArticleGalleyDAO that returns our test data.
-        import('classes.article.ArticleGalleyDAO');
-        $articleGalleyDao = $this->functionalRosettaExportTest->getMockBuilder(ArticleGalleyDAO::class)
-            ->setMethods(array('getBySubmissionId'))
-            ->getMock();
-        $articleGalleyDao->expects($this->functionalRosettaExportTest->any())
-            ->method('getBySubmissionId')
-            ->will($this->functionalRosettaExportTest->returnValue($galleys));
-        DAORegistry::registerDAO('ArticleGalleyDAO', $articleGalleyDao);
-        // FIXME: ArticleGalleyDAO::getBySubmissionId returns iterator; array expected here. Fix expectations.
-    }
+
 
     /**
      * @param Author $author
      */
-    public function createAuthors(Author $author): void
+    public function createAuthors(Submission  $submission): array
     {
+		$authors = [];
         import('classes.article.AuthorDAO');
         $authorDao = $this->functionalRosettaExportTest->getMockBuilder(AuthorDAO::class)
             ->setMethods(array('getBySubmissionId'))
             ->getMock();
-        $authorDao->expects($this->functionalRosettaExportTest->any())
-            ->method('getBySubmissionId')
-            ->will($this->functionalRosettaExportTest->returnValue(array($author)));
         DAORegistry::registerDAO('AuthorDAO', $authorDao);
+		import('classes.article.Author');
+		$author = new Author();
+		$primaryLocale = 'en_US';
+		$author->setGivenName('author-firstname', $primaryLocale);
+		$author->setFamilyName('author-lastname', $primaryLocale);
+		$author->setAffiliation('author-affiliation', $primaryLocale);
+		$author->setEmail('someone@example.com');
+		$author->setSubmissionId($submission->getId());
+		$authors[] = $author;
+		return $authors;
     }
 
     /**
@@ -137,4 +158,35 @@ class JournalTest
             ->will($this->functionalRosettaExportTest->returnValue($issue));
         DAORegistry::registerDAO('OAIDAO', $oaiDao);
     }
+	/**
+	 * @param int $journalId
+	 * @param string $context->getPrimaryLocale()
+	 */
+	public function createSubmission(Journal $context , Section  $section):  Submission
+	{
+		import('classes.submission.Submission');
+		$submission = $this->functionalRosettaExportTest->getMockBuilder(Submission::class)
+			->setMethods(array('getBestId'))
+			->getMock();
+		$submission->expects($this->functionalRosettaExportTest->any())
+			->method('getBestId')
+			->will($this->functionalRosettaExportTest->returnValue(9));
+		$submission->setId(9);
+		$submission->setJournalId($context->getId());
+		$submission->setPages(15);
+		$submission->setType('art-type', $context->getPrimaryLocale());
+		$submission->setTitle('article-title-en', $context->getPrimaryLocale());
+		$submission->setTitle('article-title-de', 'de_DE');
+		$submission->setDiscipline('article-discipline', $context->getPrimaryLocale());
+		$submission->setSubject('article-subject', $context->getPrimaryLocale());
+		$submission->setAbstract('article-abstract', $context->getPrimaryLocale());
+		$submission->setSponsor('article-sponsor', $context->getPrimaryLocale());
+		$submission->setStoredPubId('doi', 'article-doi');
+		$submission->setLanguage($context->getPrimaryLocale());
+		$submission->setSectionId($section->getId());
+		$publication = $this->createPublication($context,$submission);
+		$submission->setData('publications', [$publication]);
+		return $submission;
+	}
+
 }
--- tests/functional/FunctionalRosettaExportTestCase.php
+++ tests/functional/FunctionalRosettaExportTestCase.php
@@ -22,12 +22,12 @@ import('lib.pkp.classes.services.PKPSchemaService'); // Constants
 
 class FunctionalRosettaExportTest extends PluginTestCase
 {
-	private Journal $journal;
+	private JournalTest $journalTest;
 
 	public function __construct($name = null, array $data = [], $dataName = '')
 	{
 		parent::__construct($name, $data, $dataName);
-		$this->journal = new Journal($this);
+		$this->journalTest = new JournalTest($this);
 	}
 
 
@@ -49,45 +49,17 @@ class FunctionalRosettaExportTest extends PluginTestCase
 		$journalId = 10000;
 
 
-		// Author
-		import('classes.article.Author');
-		$author = new Author();
 		$primaryLocale = 'en_US';
-		$author->setGivenName('author-firstname', $primaryLocale);
-		$author->setFamilyName('author-lastname', $primaryLocale);
-		$author->setAffiliation('author-affiliation', $primaryLocale);
-		$author->setEmail('someone@example.com');
+		$context =$this->journalTest->createContext($primaryLocale, $journalId);
+		$issue = $this->journalTest->createIssue($context);
+		$section = $this->journalTest->createSection($context);
+		$this->journalTest->createOAI($context, $section, $issue);
 
+		$submission = $this->journalTest->createSubmission($context, $section);
+		$this->journalTest->createAuthors($submission);
+		$this->journalTest->createGalleys($submission);
 		// Article
-		import('classes.submission.Submission');
-		$article = $this->getMockBuilder(Submission::class)
-			->setMethods(array('getBestId'))
-			->getMock();
-		$article->expects($this->any())
-			->method('getBestId')
-			->will($this->returnValue(9));
-		$article->setId(9);
-		$article->setJournalId($journalId);
-		$author->setSubmissionId($article->getId());
-		$article->setPages(15);
-		$article->setType('art-type', $primaryLocale);
-		$article->setTitle('article-title-en', $primaryLocale);
-		$article->setTitle('article-title-de', 'de_DE');
-		$article->setDiscipline('article-discipline', $primaryLocale);
-		$article->setSubject('article-subject', $primaryLocale);
-		$article->setAbstract('article-abstract', $primaryLocale);
-		$article->setSponsor('article-sponsor', $primaryLocale);
-		$article->setStoredPubId('doi', 'article-doi');
-		$article->setLanguage($primaryLocale);
 
-		// Galleys
-		$galleys = $this->setGalleys();
-
-		$context = $this->createContext($primaryLocale, $journalId);
-
-		$section = $this->createSections($primaryLocale);
-
-		$issue = $this->createIssues($journalId);
 
 		// Router
 		import('lib.pkp.classes.core.PKPRouter');
@@ -112,9 +84,8 @@ class FunctionalRosettaExportTest extends PluginTestCase
 
 
 
-		$this->createAuthors($author);
-$this->createOAI($context, $section, $issue);
-		$this->createGalleys($galleys);
+
+
 
 		$importExportPlugins = PluginRegistry::loadCategory('importexport');
 		$rosettaExportPlugin = $importExportPlugins['RosettaExportPlugin'];
@@ -148,67 +119,7 @@ $this->createOAI($context, $section, $issue);
 		return array('request');
 	}
 
-	/**
-	 * @return ArticleGalley[]
-	 */
-	private function setGalleys(): array
-	{
-		return $this->journal->setGalleys();
-	}
 
-	/**
-	 * @param $context
-	 * @param Section $section
-	 * @param $issue
-	 */
-	private function createOAI($context, Section $section, $issue): void
-	{
-		$this->journal->createOAI($context, $section, $issue);
-	}
-
-	/**
-	 * @param Author $author
-	 */
-	private function createAuthors(Author $author): void
-	{
-		$this->journal->createAuthors($author);
-	}
-
-	/**
-	 * @param array $galleys
-	 */
-	private function createGalleys(array $galleys): void
-	{
-		$this->journal->createGalleys($galleys);
-	}
-
-	/**
-	 * @param int $journalId
-	 * @return Issue|mixed|\PHPUnit\Framework\MockObject\MockObject
-	 */
-	private function createIssues(int $journalId)
-	{
-		return $this->journal->createIssues($journalId);
-	}
-
-	/**
-	 * @param string $primaryLocale
-	 * @return Section
-	 */
-	private function createSections(string $primaryLocale): Section
-	{
-		return $this->journal->createSections($primaryLocale);
-	}
-
-	/**
-	 * @param string $primaryLocale
-	 * @param int $journalId
-	 * @return Journal|mixed|\PHPUnit\Framework\MockObject\MockObject
-	 */
-	private function createContext(string $primaryLocale, int $journalId)
-	{
-		return $this->journal->createContext($primaryLocale, $journalId);
-	}
 
 
 }
