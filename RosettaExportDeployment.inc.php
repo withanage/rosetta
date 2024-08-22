a4d63ddc44b5aa364f6a7e4f881aa5eb7000fd78
withanage <dulip.withanage@gmail.com>
2024-08-22 10:46:17 +0200

    Debugging

--- RosettaExportDeployment.inc.php
+++ RosettaExportDeployment.inc.php
@@ -200,7 +200,7 @@ class RosettaExportDeployment
 			'offset' => $offset
 		];
 		$endpoint = $this->getDepositEndpoint('rest') . '?' . http_build_query($params);
-		
+
 
 
 		// Define the HTTP headers
@@ -220,7 +220,7 @@ class RosettaExportDeployment
 				$body = json_decode($response->getBody(), true);
 				echo(serialize($body));
 				// Check if there are more records to fetch (pagination).
-				if ($body['total_record_count'] >= 100) {
+				if ($body['total_record_count'] >= 10) {
 					$deposits = array_merge($deposits, $this->getDepositsFromRosettaApi($offset + 1));
 				}
 
