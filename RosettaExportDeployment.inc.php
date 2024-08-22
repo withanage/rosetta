1a532a9ce7fbc0e4b8950da5c8a35fdab3ab5f42
withanage <dulip.withanage@gmail.com>
2024-08-22 09:37:46 +0200

    Debugging

--- RosettaExportDeployment.inc.php
+++ RosettaExportDeployment.inc.php
@@ -188,7 +188,7 @@ class RosettaExportDeployment
 	private function getDepositsFromRosettaApi(int $offset = 0): array
 	{
 		$deposits = []; // Array to store deposit activity data
-		
+
 
 
 		// Get the deposit endpoint
