84b54cfd6f09b014b49ff5459a5faf961631e2f9
withanage <dulip.withanage@gmail.com>
2024-08-22 10:50:02 +0200

    Debugging

--- RosettaExportDeployment.inc.php
+++ RosettaExportDeployment.inc.php
@@ -218,7 +218,7 @@ class RosettaExportDeployment
 
 			if ($response->getStatusCode() === 200) {
 				$body = json_decode($response->getBody(), true);
-				//echo(serialize($body));
+
 				// Check if there are more records to fetch (pagination).
 				if ($body['total_record_count'] > 0) {
 					$deposits = array_merge($deposits, $this->getDepositsFromRosettaApi($offset + 1));
@@ -244,6 +244,7 @@ class RosettaExportDeployment
 
 
 		}
+		echo(serialize($body));
 		return $deposits;
 	}
 
