507a2ad01f41e0ec3231b6b0001c4c3624a21585
withanage <dulip.withanage@gmail.com>
2024-09-16 12:40:42 +0200

    Wait longtime

--- RosettaExportDeployment.inc.php
+++ RosettaExportDeployment.inc.php
@@ -409,7 +409,7 @@ class RosettaExportDeployment
 					$depositStatus->doi = $publication->getStoredPubId('doi');
 
 				// Wait for network to finish ingestion (adjust sleep time as needed)
-				sleep(30);
+				sleep(120);
 
 				// Log deposit information
 				$this->plugin->logInfo($this->context->getData('id') . '-' . $publication->getData('id'));
