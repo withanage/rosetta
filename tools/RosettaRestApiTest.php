4f08c04d363e54b70fca7f926d3f227eeffc2d2c
withanage <dulip.withanage@gmail.com>
2025-01-29 15:47:23 +0100

    Tool to check API

--- tools/RosettaRestApiTest.php
+++ tools/RosettaRestApiTest.php
@@ -102,4 +102,4 @@ $settingsFile = __DIR__ . DIRECTORY_SEPARATOR . 'RosettaSettings.json';
 $depositHandler = new DepositHandler($settingsFile);
 $deposits = $depositHandler->getDepositsFromRosettaApi(0);
 
-print_r(implode(', ', $deposits);
+print_r(implode(', ', $deposits));
