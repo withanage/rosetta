87f8d543b75ea9167b9f4ac4f77e3f49ae8047a7
withanage <dulip.withanage@gmail.com>
2026-07-30 23:01:09 +0200

    amespace

--- classes/xml/mets/RosettaMETSDom.inc.php
+++ classes/xml/mets/RosettaMETSDom.inc.php
@@ -2,10 +2,10 @@
 
 namespace TIBHannover\Rosetta\Mets;
 
-import('plugins.generic.rosetta.classes.xml.mods.ModsDOM');
-import('plugins.generic.rosetta.classes.xml.XMLUtils');
-import('plugins.generic.rosetta.classes.xml.dublincore.RosettaDCDom');
-import('plugins.generic.rosetta.classes.files.RosettaFileService');
+import('plugins.importexport.rosetta.classes.xml.mods.ModsDOM');
+import('plugins.importexport.rosetta.classes.xml.XMLUtils');
+import('plugins.importexport.rosetta.classes.xml.dublincore.RosettaDCDom');
+import('plugins.importexport.rosetta.classes.files.RosettaFileService');
 
 define('MASTER_PATH', 'MASTER');
 
